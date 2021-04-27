4c23000e89bc3bb384c3277ad671efe165d10349
Truong Dao <ton.truong.dao@outlook.com>
2021-04-27 17:06:59 -0400

    Fetch data with serverless function

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,41 @@
 import Layout from '@/components/Layout'
+import { API_URL } from "@/config/index"
 
-const HomePage = () => {
+const HomePage = ({ events }) => {
+  
+  // console.log(events)
+
   return (
     <div>
         <Layout>
-          <h1>Home</h1>
+          <h1>Upcoming Events</h1>
         </Layout>
     </div>
   )
 }
 
 export default HomePage
+
+
+export const getStaticProps = async () => {
+  const res = await fetch(`${API_URL}/api/events`)
+  const events = await res.json()
+
+  return {
+    props: {events},
+    revalidate: 1
+  }
+}
+
+
+// This works as well
+
+// export const getServerSideProps = async () => {
+//   const res = await fetch(`${API_URL}/api/events`)
+//   const events = await res.json()
+
+//   return {
+//     props: {events},
+//   }
+// }
 
