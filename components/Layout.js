64f6dce8cefe16a7a74ac1a1efdd3fac4e867644
Truong Dao <ton.truong.dao@outlook.com>
2021-04-27 16:18:10 -0400

    Added Showcase comp & use conditional remdering

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,15 @@
 import Head  from 'next/head'
+import { useRouter } from 'next/router'
+
 import Header from './Header'
 import Footer from './Footer'
-import styles from '../styles/Layout.module.css'
+import Showcase from './Showcase'
+import styles from '@/styles/Layout.module.css'
 
 const Layout = ({ title, keywords, description, children }) => {
+  
+  const router = useRouter()
+
   return (
     <div>
       <Head>
@@ -13,6 +19,7 @@ const Layout = ({ title, keywords, description, children }) => {
       </Head>
 
       <Header/>
+      {router.pathname === '/' && <Showcase/>}
       <div className={styles.container}>
         {children}
       </div>
---
+++ components/Showcase.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from '@/styles/Showcase.module.css'
+
+const Showcase = () => {
+  return (
+    <div className={styles.showcase}>
+      <h1>Welcome!</h1>
+      <div>Find the best events</div>
+    </div>
+  )
+}
+
+export default Showcase
