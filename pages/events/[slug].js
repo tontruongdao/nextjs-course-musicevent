c0dc7ccaffb192eb9af0e2d13487baeea17b5861
Truong Dao <ton.truong.dao@outlook.com>
2021-04-27 12:29:49 -0400

    Clean up Home and tested "useRouter"  hook

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,9 +1,15 @@
 import React from 'react'
+import { useRouter } from 'next/router'
 
 const EventById = () => {
+
+  const router = useRouter()
+  console.log(router);
+
   return (
     <div>
-      Event By Id
+      <h1>Event By Id</h1>
+      <button onClick={() => router.push('/')}>Home</button>
     </div>
   )
 }
