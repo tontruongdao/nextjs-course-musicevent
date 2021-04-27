e8acee31ffb4a2837f62b5e6d9efa9bb85b57ada
Truong Dao <ton.truong.dao@outlook.com>
2021-04-27 15:39:26 -0400

    Added react/icons pack and customizd 404

---
+++ pages/404.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Link from 'next/link'
+import { FaExclamationTriangle } from 'react-icons/fa'
+
+import Layout from '../components/Layout'
+import styles from '../styles/404.module.css'
+
+const NotFoundPage = () => {
+  return (
+    <Layout title='Page Not Found'>
+      <div className={styles.error}>
+        <h1><FaExclamationTriangle/>404</h1>
+        <div>Sorry, there is nothing here</div>
+        <Link href='/'>Back To Homepage!</Link>
+      </div>
+    </Layout>
+    
+  )
+}
+
+export default NotFoundPage
