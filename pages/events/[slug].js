71bb3ef671f17c618df34f5c1eb6c01b5a9d4978
Truong Dao <ton.truong.dao@outlook.com>
2021-04-28 13:40:30 -0400

    Ftch single event with staticProps & staticPaths

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,19 +1,65 @@
 import { useRouter } from 'next/router'
-import Layout from '@/components/Layout'
 
-const EventById = () => {
+import Layout from '@/components/Layout'
+import { API_URL } from '@/config/index'
+
+const EventPage = ({ evt }) => {
 
   const router = useRouter()
-  console.log(router);
+  // console.log(router);
 
   return (
     <Layout>
       <div>
-        <h1>Event By Id</h1>
+        <h1>{evt.name}</h1>
         <button onClick={() => router.push('/')}>Home</button>
       </div>
     </Layout>
   )
 }
 
-export default EventById
+export default EventPage
+
+
+export const getStaticPaths= async() => {
+
+  const res = await fetch(`${API_URL}/api/events`)
+  const events = await res.json()
+
+  const paths = events.map(evt => ({
+    params: { slug: evt.slug }
+  }))
+
+  return {
+    paths,
+    fallback: false // Will return 404 if there is no path.
+  }
+}
+
+
+export const getStaticProps = async ({ params: { slug } }) => {
+
+  const res = await fetch(`${API_URL}/api/events/${slug}`)
+  const events = await res.json()
+  // console.log(events)
+
+  return {
+    props: { 
+      evt: events[0] ,
+      revalidate: 1
+    }
+  }
+}
+
+// export const getServerSideProps = async ({ query: { slug } }) => {
+
+//   const res = await fetch(`${API_URL}/api/events/${slug}`)
+//   const events = await res.json()
+//   // console.log(events)
+
+//   return {
+//     props: { 
+//       evt: events[0] 
+//     }
+//   }
+// }
