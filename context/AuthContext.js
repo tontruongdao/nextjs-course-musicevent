00153b9ea241433d17b0fcba2af0decfc3283b89
Truong Dao <ton.truong.dao@outlook.com>
2021-05-01 13:24:09 -0400

    Created basic template for AuthContext

---
+++ context/AuthContext.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect, createContext } from 'react'
+import { useRouter } from 'next/router'
+import { API_URL } from '@/config/index'
+
+const AuthContext = createContext()
+
+
+
+const AuthProvider = ({ children }) => {
+  // ##### React State
+  const [ user, setUser ] = useState(null)
+  const [ error, setError ] = useState(null)
+
+
+  // ##### Helper Functions
+  
+  // Register User
+  const register = async (user) => {
+    console.log(user)
+  } 
+
+  // Login User
+  // Renames the email to "identifier", to match strappi configuration
+  const login = async ({ email:identifier, password}) => { 
+    console.log({ identifier, password})
+  }
+
+  // Logout User
+  const logout = async () => {
+    console.log('Logout')
+  }
+
+  // Check if User is Logged In.
+  const checkUserLoggedIn = async (user) => {
+    console.log('Checked')
+  }
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AuthProvider
